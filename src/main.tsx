cc9eabbe9ad026729885e3f003b8737484470bd7
Willian Almeida <willianac01@gmail.com>
2024-01-15 16:27:35 -0300

    feat: added routes using react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { RouterProvider } from 'react-router-dom';
+import { router } from "./routes.tsx";
 
 const theme = createTheme({
   palette: {
@@ -39,7 +40,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-			<App />
+			<RouterProvider router={router}/>
 		</ThemeProvider>
   </React.StrictMode>,
 )
