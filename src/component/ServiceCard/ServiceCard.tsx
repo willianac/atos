337a798e1b14c09353d6dfe2b8f808563086ee50
Willian Almeida <willianac01@gmail.com>
2024-01-18 19:50:20 -0300

    feat: created a service card to use in homepage

---
+++ src/component/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,36 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+type ServiceCardProps = {
+	image: string
+	title: string
+	body: string
+	actionUrl: string
+}
+
+function ServiceCard({ actionUrl, body, image, title }: ServiceCardProps) {
+	return (
+    <Card sx={{ maxWidth: 350 }}>
+      <CardMedia
+        sx={{ height: 300 }}
+        image={image}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+					{body}
+        </Typography>
+      </CardContent>
+      <CardActions>
+				<Link to={actionUrl}>
+          <Button size="small">Saiba mais</Button>
+				</Link>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default ServiceCard;
