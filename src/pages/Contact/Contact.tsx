f6eac3130182220b8ce12828858883c9bb7a9ea6
Willian Almeida <willianac01@gmail.com>
2024-01-25 11:08:35 -0300

    feat: UI rework on contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert"
+import { useSpring, animated, config } from "@react-spring/web";
 
 function Contact() {
 	const [open, setOpen] = useState(false)
@@ -20,20 +21,45 @@ function Contact() {
 		setOpen(false)
 	}
 
+	const leftSlideAnimation = useSpring({
+		from: {x: -400},
+		to: {x: 0},
+		config: config.slow
+	})
+	const rightSlideAnimation = useSpring({
+		from: {x: 400},
+		to: {x: 0},
+		config: config.slow
+	})
+
 	return (
 		<>
-			<section className="py-8 px-3.5 bg-zinc-200 flex flex-col justify-between items-center lg:py-16 lg:flex-row lg:px-64">
+			<section className="py-8 px-3.5 bg-zinc-200 flex flex-col justify-between items-center md:px-32 lg:py-16 lg:flex-row xl:px-64">
 				<div>
 					<h1 className="text-zinc-700 text-3xl font-bold lg:text-4xl">Entre em contato conosco</h1>
 					<span className="text-lg text-zinc-700">Ou nos mande um email em um de nossos endereços:</span>
 				</div>
-				<div className="flex flex-col items-center w-full lg:w-auto mt-5">
-					<a href="https://wa.me/5521964407377" target="_blank" rel="noopener noreferrer" className="w-full">
+				<div className="flex w-full lg:w-auto">
+					<a href="https://wa.me/5521964407377" target="_blank" rel="noopener noreferrer" className="w-full md:w-96">
 						<Button variant="contained" color="success" size="large" className="w-full" startIcon={<FaWhatsapp />}>Whatsapp</Button>
 					</a>
-					<span className="text-indigo-600 text-lg hover:underline cursor-pointer mt-4" ref={email1Ref} onClick={() => copyEmail(email1Ref)}>atoselevadores@hotmail.com</span>
-					<span className="text-indigo-600 text-lg hover:underline cursor-pointer" ref={email2Ref} onClick={() => copyEmail(email2Ref)}>comercial.ats@gmail.com.br</span>
 				</div>
+			</section>
+			<section className="flex flex-col justify-between md:px-32 gap-y-4 lg:flex-row xl:px-64 my-4">	
+				<animated.div style={leftSlideAnimation} className="bg-white rounded-xl w-full p-10 shadow-md shadow-black/10 lg:w-96">
+					<h1 className="text-2xl font-bold text-zinc-800 text-center">Outros telefones</h1>
+					<div className="flex flex-col items-center text-lg text-zinc-800 mt-4">
+						<span>(21) 4119-7888</span>
+						<span>(21) 97008-4583</span>
+					</div>
+				</animated.div>	
+				<animated.div style={rightSlideAnimation} className="bg-white rounded-xl w-full p-10 shadow-md shadow-black/10 lg:w-96">
+					<h1 className="text-2xl font-bold text-zinc-800 text-center">Envie-nos um email</h1>
+					<div className="flex flex-col items-center">
+						<span className="text-indigo-600 text-lg hover:underline cursor-pointer mt-4" ref={email1Ref} onClick={() => copyEmail(email1Ref)}>atoselevadores@hotmail.com</span>
+						<span className="text-indigo-600 text-lg hover:underline cursor-pointer" ref={email2Ref} onClick={() => copyEmail(email2Ref)}>comercial.ats@gmail.com.br</span>
+					</div>
+				</animated.div>	
 			</section>
 			<Snackbar 
 				open={open}
